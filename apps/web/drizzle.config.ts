2eea961bb871ffece30b22bcb97dddef0aedd4fe
Killer545537 <srajanmahajan1@gmail.com>
2026-02-15 02:45:18 +0530

    chore: fixing default configs

--- apps/web/drizzle.config.ts
+++ apps/web/drizzle.config.ts
@@ -1,13 +1,21 @@
-import { config } from 'dotenv'
-import { defineConfig } from 'drizzle-kit'
+import { config } from 'dotenv';
+import { defineConfig } from 'drizzle-kit';
 
-config({ path: ['.env.local', '.env'] })
+config({ path: ['.env.local', '.env'] });
 
 export default defineConfig({
-  out: './drizzle',
-  schema: './src/db/schema.ts',
-  dialect: 'postgresql',
-  dbCredentials: {
-    url: process.env.DATABASE_URL,
-  },
-})
+    out: './drizzle',
+    schema: './src/db/schema.ts',
+    dialect: 'postgresql',
+    migrations: {
+        schema: 'public',
+        table: '__drizzle_migrations',
+    },
+    dbCredentials: {
+        url: process.env.DATABASE_URL!,
+    },
+
+    casing: 'snake_case',
+    strict: true,
+    verbose: true,
+});
